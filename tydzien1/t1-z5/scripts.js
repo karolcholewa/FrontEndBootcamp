18359c2eb47359d27587ecedf97f601d713ec4d4
Carlito <karol.ch@gmail.com>
2017-06-25 23:27:45 +0200

    Used Handlebar.js instead of Mustache.js
    	# Changes to be committed:

--- tydzien1/t1-z5/scripts.js
+++ tydzien1/t1-z5/scripts.js
@@ -1,49 +1,43 @@
 (function ($) {
 
-    'use strict';
+        'use strict';
 
-    var myData = $("#data"),
-        items = [],
-        template = $("#template").html(),
-        DATA_URL = "https://jsonplaceholder.typicode.com/users";
+        var myData = $("#data"),
+            items = [],
+            templateCode = $("#template").html(), // Pobieram HTML z szablonem
+            template = Handlebars.compile(templateCode), //Kompiluje szablon
+            DATA_URL = "https://jsonplaceholder.typicode.com/users";
 
 
-    function showUsers(data) {
+        function showUsers(data) {
 
-        var output = "";
 
-        $.each(data, function (key, val) {
 
-            output += Mustache.render(template, val)
+            var output = "";
+            $.each(data, function (key, val) {
+                output += template(val);
+                
+            });
 
-        });
-        
-        $("#data").append( $("<ul>").html(output));
-        
-        console.log(output);
+            $("#data").append($("<ul>").html(output));
 
+       
 
     }
 
     $("#button").on("click", function () {
 
-        console.log("klik");
+
 
         $.getJSON(DATA_URL)
             .done(function (data) {
 
-
-
-                console.log(data);
                 showUsers(data);
-
-
 
             })
             .fail(function () {
                 alert("cos poszlo nie tak");
             });
-
 
     });
 
