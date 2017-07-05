f241081c0d04bf563781519b43fa2f72333bf19e
Carlito <karol.ch@gmail.com>
2017-07-05 22:14:12 +0200

    initial commit

--- tydzien3/t3-z1/scripts.js
+++ tydzien3/t3-z1/scripts.js
@@ -1,27 +1,30 @@
 (function () {
-    
-    var button = document.querySelector("#showMapLink");
-    
-    button.addEventListener("click", function(e){
-        
-        if(!navigator.geolocation) {
-            
-            return;
-        }
-        
-        button.textContent = "Czekaj...";
-        button.setAttribute("disabled", true);
-        
-        navigator.geolocation.getCurrentPosition(function(position){
-           
-            var coords = position.coords,
-                maplink = document.createElement("a");
-            
-            maplink.classList.add("button");
-            maplink.setAttribute("href", "http://bing.com/maps/defautl.aspx?cp=" + coords.latitude + "~" + coords.langitude);
-            
-            maplink.textContent = "Zobacz na mapie";
-        button.parentNode.appendChild("div");
-        });
-    })
+
+	var button = document.querySelector("#showMapLink");
+
+	button.addEventListener("click", function (e) {
+
+		//checking if geolocalisation is supported by the browser
+		if (!navigator.geolocation) {
+
+			return;
+		}
+
+		this.textContent = "Czekaj...";
+		this.setAttribute("disabled", true);
+
+		navigator.geolocation.getCurrentPosition(function (position) {
+
+			var coords = position.coords,
+				maplink = document.createElement("a");
+
+			maplink.classList.add("button");
+			maplink.setAttribute("href", "https://www.bing.com/maps/default.aspx?cp=" + coords.latitude + "~" + coords.langitude);
+
+			maplink.textContent = "Zobacz na mapie";
+			button.parentNode.appendChild(maplink);
+			button.parentNode.removeChild(button);
+
+		});
+	})
 })();
